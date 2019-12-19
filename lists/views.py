97a708bb6e95854820011977071bc22f340cea6d
sebastian <leehur_sebastian@hotmail.com>
2019-12-19 11:39:26 +0800

    Created new URL to process form, simplified home page

--- lists/views.py
+++ lists/views.py
@@ -4,10 +4,6 @@ from .models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -17,3 +13,8 @@ def view_list(request):
         'items': items
     }
     return render(request, 'list.html', context)
+
+def new_list(request):
+    new_item_text = request.POST['item_text']
+    Item.objects.create(text=new_item_text)
+    return redirect('/lists/the-only-list-in-the-world/')

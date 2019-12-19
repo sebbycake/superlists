5de69c2f2baa4791fb547c1cb914a9e9ca20b31e
sebastian <leehur_sebastian@hotmail.com>
2019-12-19 15:06:06 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -6,11 +6,10 @@ from .models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
     context = {
-        'items': items
+        'list': list_
     }
     return render(request, 'list.html', context)
 
@@ -18,4 +17,9 @@ def new_list(request):
     list_ = List.objects.create()
     new_item_text = request.POST['item_text']
     Item.objects.create(text=new_item_text, list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{list_.id}/')
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')

a296a965b8a15924040644a1059a153cd5ec1586
sebastian <leehur_sebastian@hotmail.com>
2019-12-24 11:53:21 +0800

    Split out unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+# Create your tests here.
+
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retreiving_items(self):
+        # new list object aka new row in db
+        list_ = List()
+        # add new row to db
+        list_.save()
+
+        # new to-do item
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        # related field list. every item has a list
+        first_item.list = list_
+        first_item.save()
+
+        # second to-do item
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,13 +2,11 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 
-from .views import home_page
+from lists.views import home_page
 
-from .models import Item, List
+from lists.models import Item, List
 
 # Create your tests here.
-
-
 class HomePageTest(TestCase):
 
     # def test_root_url_resolves_to_home_page_view(self):
@@ -35,42 +33,6 @@ class HomePageTest(TestCase):
 
     #     self.assertIn('itemey 1', response.content.decode())
     #     self.assertIn('itemey 2', response.content.decode())
-
-
-class ListAndItemModelsTest(TestCase):
-
-    def test_saving_and_retreiving_items(self):
-        # new list object aka new row in db
-        list_ = List()
-        # add new row to db
-        list_.save()
-
-        # new to-do item
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        # related field list. every item has a list
-        first_item.list = list_
-        first_item.save()
-
-        # second to-do item
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
 
 class ListViewTest(TestCase):
 

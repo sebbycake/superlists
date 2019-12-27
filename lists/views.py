c52e7729c439a1e0b77574948d7fa337e00580d7
sebastian <leehur_sebastian@hotmail.com>
2019-12-27 13:31:12 +0800

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -10,6 +10,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     context = {
         'list': list_
     }
@@ -21,6 +24,7 @@ def new_list(request):
     item = Item.objects.create(text=new_item_text, list=list_)
     try:
         item.full_clean()
+        item.save()
     except ValidationError:
         list_.delete()
         error = "You can't have an empty list item"
@@ -30,7 +34,3 @@ def new_list(request):
         return render(request, 'home.html', context)
     return redirect(f'/lists/{list_.id}/')
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')

4d91de963fa2c63a97e0d7aca10f0c6196e07c00
sebastian <leehur_sebastian@hotmail.com>
2020-01-02 14:12:06 +0800

    Validated duplicated items with forms

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,7 +8,9 @@ from lists.views import home_page
 
 from lists.models import Item, List
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (ItemForm, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, DUPLICATE_ITEM_ERROR,
+)
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -99,12 +101,12 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
+    
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
         response = self.client.post(f'/lists/{list1.id}/', data={'text': 'textey'})
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)

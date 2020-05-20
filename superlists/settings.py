af1f2b9afadf29efb5fb56282f9c655dbb408f21
sebastian <leehur_sebastian@hotmail.com>
2020-05-20 13:19:57 +0800

    Updated settings for Heroku production

--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7*_xy4hnylt+xd9vg-f=!yd!y3=czpqc&x4cs4o^-^t*o^yp7w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = [
-    '*'
+    'localhost',
+    'http://listeefy.sebbycake.com',
+    'https://listeefy.herokuapp.com',
 ]
 
 
@@ -134,8 +137,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # collected static files go here: python manage.py collectstatic
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+django_heroku.settings(locals())
